6aaa2f388181262a3e3c4220a4f948fedbe21830
bahiim <abde2001raja@gmail.com>
2024-06-07 18:27:35 -0700

    fe

---
+++ dev/hbtn_tests/test_save_reload_user.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/python3
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+
+all_objs = storage.all()
+print("-- End Reloaded objects --")
+print("printing values only of reloaded:")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Begin Create a new User --")
+my_user = User()
+my_user.first_name = "abderrahim"
+my_user.last_name = "oulmaamer"
+my_user.email = "abde2001raja@gmail.com"
+my_user.password = "root"
+my_user.save()
+an_bm = BaseModel()
+an_bm.save()
+print(my_user)
+
